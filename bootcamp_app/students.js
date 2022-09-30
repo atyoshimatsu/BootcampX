39b13f06108284398745ca3b2897205ea3527c74
atyoshimatsu <at.yoshimatsu@gmail.com>
2022-09-30 00:13:49 -0700

    Create students.js

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -1,4 +1,5 @@
 const { Pool } = require('pg');
+const argv = process.argv;
 
 const pool = new Pool({
   user: 'vagrant',
@@ -6,3 +7,18 @@ const pool = new Pool({
   host: 'localhost',
   database: 'bootcampx'
 });
+
+const [cohortName, limit] = argv.slice(2);
+const whereClause = !!cohortName ? `WHERE c.name like '%${cohortName}%'` : '';
+pool.query(`
+SELECT s.id AS id, s.name AS name, c.name AS cohort_name
+FROM students AS s
+JOIN cohorts AS c ON s.cohort_id = c.id
+${whereClause}
+LIMIT ${limit || 5};
+`)
+.then(res => {
+  res.rows.forEach(user => {
+    console.log(`${user.name} has an id of ${user.id} and was in the ${user.cohort_name} cohort`);
+  })
+}).catch(err => console.error('query error', err.stack));
